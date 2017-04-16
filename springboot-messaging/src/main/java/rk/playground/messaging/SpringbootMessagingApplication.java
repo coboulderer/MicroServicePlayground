2d96efda7a997d22c06bc760f92cafddd4cd0f05
Rick K <rick.kowaleski@gmail.com>
2017-04-15 21:33:19 -0600

    RabbitMQ Example

--- springboot-messaging/src/main/java/rk/playground/messaging/SpringbootMessagingApplication.java
+++ springboot-messaging/src/main/java/rk/playground/messaging/SpringbootMessagingApplication.java
@@ -1,12 +1,22 @@
 package rk.playground.messaging;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootMessagingApplication {
+public class SpringbootMessagingApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMessagingApplication.class, args);
-	}
+    @Autowired
+    Sender sender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMessagingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        sender.send("Hello From RabbitMQ");
+    }
 }

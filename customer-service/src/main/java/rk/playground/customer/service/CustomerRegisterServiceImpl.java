6aea6fba1ce8995a63d048972f6f4668502d93a0
Rick K <rick.kowaleski@gmail.com>
2017-04-19 21:11:32 -0600

    create Sender service which uses RabbitMQ

--- customer-service/src/main/java/rk/playground/customer/service/CustomerRegisterServiceImpl.java
+++ customer-service/src/main/java/rk/playground/customer/service/CustomerRegisterServiceImpl.java
@@ -3,6 +3,7 @@ package rk.playground.customer.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import rk.playground.customer.dao.CustomerDao;
@@ -12,13 +13,16 @@ import rk.playground.customer.domain.Customer;
  * Implementation for CustomerRegisterService
  */
 @Service
+@Lazy
 public class CustomerRegisterServiceImpl implements CustomerRegisterService{
 
-    CustomerDao customerDao;
+    private CustomerDao customerDao;
+    private Sender sender;
 
     @Autowired
-    public CustomerRegisterServiceImpl(CustomerDao customerDao) {
+    public CustomerRegisterServiceImpl(CustomerDao customerDao, Sender sender) {
         this.customerDao = customerDao;
+        this.sender = sender;
     }
 
     public Customer register(Customer customer) {
@@ -28,6 +32,7 @@ public class CustomerRegisterServiceImpl implements CustomerRegisterService{
                     "exists");
         } else {
             customerDao.save(customer);
+            sender.send(customer.getEmail());
         }
         return customer;
     }
